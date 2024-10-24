02b83dfba1a87857b79db04b9a9ca5106093bd27
Han Min Thant <hanminthant222@gmail.com>
2024-10-25 00:55:39 +0800

    h

--- so_long/so_long.h
+++ so_long/so_long.h
@@ -2,23 +2,8 @@
 #define SO_LONG_H
 
 #include "./mlx/mlx.h"
-#include "./libft/libft.h"
+#include "./get_next_line/get_next_line.h"
 #include <fcntl.h>
-#include <stdlib.h>
-#include <stdio.h>
-
-#define IMG_W 32
-#define IMG_H 32
-#define ESC 53
-
-typedef struct s_map
-{
-	char	**map;
-	void	*object;
-	int		x;
-	int		y;
-	int		sheeps;
-}map_data;
 
 typedef struct s_data
 {
@@ -29,36 +14,13 @@ typedef struct s_data
 	int endian;
 } img_data;
 
-typedef struct s_window
-{
-	int	width;
-	int	height;
-	map_data *map;
-}window_data;
-
 typedef struct m_data
 {
 	void *mlx;
 	void *win;
-
-	img_data *img;
 } mlx_data;
 
 int close_window(mlx_data *vars);
 int handle_keypress(int key, mlx_data *vars);
-
-// map.c
-void	ft_input_error(int	argc, int fd);
-int is_ber_file(const char *filename);
-void    get_window_size(window_data *window, char **av);
-void	read_lines_to_map(window_data *window, int fd, char **map);
-void	ft_ber_to_array(window_data *window, int argc, char **argv);
-
-// map_checks
-void	ft_check_border(window_data *window);
-void    ft_check_map(window_data *window);
-void	ft_check_rectangular(window_data *window);
-void    ft_print_error_msg(const char *error_msg);
-void    ft_check_contents(window_data *window);
 
 #endif
---
+++ so_long/src/main.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include "../so_long.h"
+
+int main(void)
+{
+	mlx_data mlx;
+	img_data dino_img, wall_img, cave_img, sheep_img;
+	int width = 25;
+	int height = 25;
+
+	mlx.mlx = mlx_init();
+	mlx.win = mlx_new_window(mlx.mlx, 680, 800, "Hello world!");
+	if (!mlx.mlx || !mlx.win)
+	{
+		printf("Error initializing MLX or creating window\n");
+		return (1);
+	}
+
+	// Load the Dino image
+	dino_img.img = mlx_xpm_file_to_image(mlx.mlx, "./images/dino.xpm", &width, &height);
+	if (!dino_img.img)
+	{
+		printf("Error loading image dino.xpm\n");
+		return (1);
+	}
+	mlx_put_image_to_window(mlx.mlx, mlx.win, dino_img.img, 0, 0);
+
+	// Load the Wall image
+	wall_img.img = mlx_xpm_file_to_image(mlx.mlx, "./images/wall.xpm", &width, &height);
+	if (!wall_img.img)
+	{
+		printf("Error loading image wall.xpm\n");
+		return (1);
+	}
+	mlx_put_image_to_window(mlx.mlx, mlx.win, wall_img.img, 50, 0);
+
+	// Load the Cave image
+	cave_img.img = mlx_xpm_file_to_image(mlx.mlx, "./images/cave.xpm", &width, &height);
+	if (!cave_img.img)
+	{
+		printf("Error loading image cave.xpm\n");
+		return (1);
+	}
+	mlx_put_image_to_window(mlx.mlx, mlx.win, cave_img.img, 100, 0);
+
+	// Load the Sheep image
+	sheep_img.img = mlx_xpm_file_to_image(mlx.mlx, "./images/sheep.xpm", &width, &height);
+	if (!sheep_img.img)
+	{
+		printf("Error loading image sheep.xpm\n");
+		return (1);
+	}
+	mlx_put_image_to_window(mlx.mlx, mlx.win, sheep_img.img, 150, 0);
+
+	// Hook to handle key presses and close window
+	// mlx_hook(mlx.win, 2, 1L << 0, handle_keypress, &mlx);
+
+	// Hook to handle window close event (clicking the close button)
+	mlx_hook(mlx.win, 17, 0, close_window, &mlx);
+
+	// Start the MLX loop
+	mlx_loop(mlx.mlx);
+}
