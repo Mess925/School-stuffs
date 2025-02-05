7fb229c1c7f4e5cedb9e779a77846d652e2eac12
Han Min Thant <hanminthant222@gmail.com>
2025-02-05 17:54:32 +0800

    minishell

--- minishell/src/execute/exec_path.c
+++ minishell/src/execute/exec_path.c
@@ -6,7 +6,7 @@
 /*   By: ysetiawa <ysetiawa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/14 17:22:59 by yocelynnns        #+#    #+#             */
-/*   Updated: 2025/02/04 20:51:30 by ysetiawa         ###   ########.fr       */
+/*   Updated: 2025/02/05 17:12:14 by ysetiawa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -101,6 +101,12 @@ int	fork_and_execute(t_ast_node *ast, char **env, t_minishell *mini,
 
 	if (handle_builtin_commands(ast, mini) == 0)
 		return g_sig.exit_value;
+	if (is_directory(ast->command->args[0]))
+	{
+		g_sig.exit_value = 126;
+		printf("minishell: %s: Is a directory\n", ast->command->args[0]);
+		return g_sig.exit_value;
+	}
 	pid = fork();
 	if (pid == 0)
 	{
