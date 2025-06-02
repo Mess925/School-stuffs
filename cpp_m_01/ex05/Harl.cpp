01c6932eb0139c5824cf9f5fe27a7ad107482591
Mess925 <hanminthant222@gmail.com>
2025-06-02 17:18:11 +0800

    ex05

--- cpp_m_01/ex05/Harl.cpp
+++ cpp_m_01/ex05/Harl.cpp
@@ -6,25 +6,49 @@
 /*   By: hthant <hthant@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 16:50:31 by hthant            #+#    #+#             */
-/*   Updated: 2025/06/02 16:52:47 by hthant           ###   ########.fr       */
+/*   Updated: 2025/06/02 17:16:07 by hthant           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "Harl.hpp"
+#include "Harl.hpp"
 
-void Harl::debug(){
-    
-};
-void Harl::info(){
-    
-};
-void Harl::warning(){
-    
-};
-void Harl::error(){
-    
-};
+void Harl::debug(void) {
+    std::cout << "[ DEBUG ]\n";
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!\n\n";
+}
 
-void Harl::complain(std::string level){
-    
+void Harl::info(void) {
+    std::cout << "[ INFO ]\n";
+    std::cout << "I cannot believe adding extra bacon costs more money.\n";
+    std::cout << "You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!\n\n";
+}
+
+void Harl::warning(void) {
+    std::cout << "[ WARNING ]\n";
+    std::cout << "I think I deserve to have some extra bacon for free.\n";
+    std::cout << "I've been coming for years, whereas you started working here just last month.\n\n";
+}
+
+void Harl::error(void) {
+    std::cout << "[ ERROR ]\n";
+    std::cout << "This is unacceptable! I want to speak to the manager now.\n\n";
+}
+
+void Harl::complain(std::string level) {
+    std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+    void (Harl::*functions[])(void) = {
+        &Harl::debug,
+        &Harl::info,
+        &Harl::warning,
+        &Harl::error
+    };
+
+    for (int i = 0; i < 4; i++) {
+        if (levels[i] == level) {
+            (this->*functions[i])();
+            return;
+        }
+    }
+
+    std::cout << "[ UNKNOWN LEVEL ]\nHarl has nothing to say...\n";
 }
