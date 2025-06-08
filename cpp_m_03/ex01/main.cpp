39171393c0a7ab900d36268377aee8c0f0a233a0
Mess925 <hanminthant222@gmail.com>
2025-06-08 20:49:07 +0800

    stupid delivered clas cpp03ex03

--- cpp_m_03/ex01/main.cpp
+++ cpp_m_03/ex01/main.cpp
@@ -6,11 +6,12 @@
 /*   By: hthant <hthant@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/07 09:48:49 by hthant            #+#    #+#             */
-/*   Updated: 2025/06/08 20:41:20 by hthant           ###   ########.fr       */
+/*   Updated: 2025/06/08 20:43:51 by hthant           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
+# include "ScavTrap.hpp"
 
 int main() {
     std::cout <<std::endl <<"_______________Creating Original ClapTrap___________________" << std::endl;
@@ -43,8 +44,39 @@ int main() {
     copy.beRepaired(5);
     copy.takeDamage(5);
     copy.beRepaired(5);
-    copy.takeDamage(6);
+    copy.takeDamage(10);
     copy.beRepaired(10);
-    std::cout <<std::endl <<"__________________________END OF TEST_________________________"<<std::endl;
+    std::cout <<std::endl <<"__________________________END OF CLAPTRAP TEST_______________"<<std::endl << std::endl;
 
+
+
+
+
+    std::cout <<std::endl <<"_______________Creating Original ScavTrap___________________" << std::endl;
+    ScavTrap soriginal ("A");
+    
+    std::cout <<std::endl <<"_________________Creating Copy Scavtrap_____________________" << std::endl;
+    ScavTrap scopy = soriginal;
+    
+    std::cout <<std::endl <<"______________Assgning the original Scavtrap________________" <<std::endl;
+    soriginal.attack("B");
+    soriginal.takeDamage(5);
+    soriginal.beRepaired(1);
+
+    std::cout <<std::endl <<"______________________ENERGY POINT TEST______________________" <<std::endl;
+    ScavTrap s ("C");
+    for (int i = 0; i < 55; i++)
+    {
+        s.attack("B");
+    }
+    s.guardGate();
+    
+    std::cout <<std::endl <<"______________________REPARE AND DIE TEST____________________" <<std::endl;
+    scopy.takeDamage(90);
+    scopy.beRepaired(5);
+    scopy.takeDamage(5);
+    scopy.beRepaired(5);
+    scopy.takeDamage(15);
+    scopy.beRepaired(10);
+    std::cout <<std::endl <<"__________________________END OF CLAPTRAP TEST_______________"<<std::endl;
 }
