dea5bf0c167bce122b84597f7cb1922b68b19520
Mess925 <hanminthant222@gmail.com>
2025-06-25 06:08:51 +0800

    exam sandbox update

--- 4xam/lvl00/sandbox/sandbox.c
+++ 4xam/lvl00/sandbox/sandbox.c
@@ -1,86 +1,91 @@
 
 #include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <errno.h>
-#include <string.h>
-#include <unistd.h>
+#include <stdio.h>
 #include <signal.h>
+#include <string.h>
+#include <stdlib.h>
 
-int g_to;
-
-void handle_timeout(int sig)
+static void do_nothing(int sig)
 {
-    if (SIGALRM == sig)
-        g_to = 1;
+    (void)sig; // unused parameter
 }
 
 int sandbox(void (*f)(void), unsigned int timeout, bool verbose)
 {
-    int status;
-    pid_t pid;
-    struct sigaction sa;
-
-    pid = fork();
+    pid_t pid = fork();
     if (pid == -1)
-        return (-1);
+        return -1;
+
     if (pid == 0)
     {
+        alarm(timeout);
         f();
         exit(0);
     }
-    sa.sa_handler = handle_timeout;
-    sigaction(SIGALRM, &sa, NULL);
+
+    struct sigaction sa;
+    sa.sa_handler = do_nothing;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    if (sigaction(SIGALRM, &sa, NULL) == -1)
+        return -1;
+
     alarm(timeout);
-    if (waitpid(pid, &status, 0) == -1)
+
+    int status;
+    pid_t r = waitpid(pid, &status, 0);
+    if (r == -1)
     {
-        while (1)
+        if (errno == EINTR)
         {
-            if (errno == EINTR)
-            {
-                if (g_to)
-                {
-                    if (verbose)
-                        printf("Bad function: timed out in %u seconds\n", timeout);
-                    kill(pid, SIGKILL);
-                    if (waitpid(pid, &status, 0) == -1)
-                        return (-1);
-                    return (0);
-                }
-                continue;
-            }
-            else
-                return (-1);
-            break;
+            kill(pid, SIGKILL);
+            waitpid(pid, NULL, 0);
+            if (verbose)
+                printf("Bad function: timed out after %u seconds\n", timeout);
+            return 0;
         }
+        return -1;
     }
-    if (WIFSIGNALED(status))
-    {
-        int exit_code = WTERMSIG(status);
-        if (verbose)
-            printf("Bad function: %s\n", strsignal(exit_code));
-        return (0);
-    }
+
     if (WIFEXITED(status))
     {
         int exit_code = WEXITSTATUS(status);
-        if (exit_code)
+        if (exit_code == 0)
         {
             if (verbose)
-                printf("Bad function: exited with code %d\n", exit_code);
-            return (0);
+                printf("Nice function!\n");
+            return 1;
         }
         else
         {
             if (verbose)
-                printf("Nice function!\n");
-            return (1);
+                printf("Bad function: exited with code %d\n", exit_code);
+            return 0;
         }
     }
-    return (-1);
+    else if (WIFSIGNALED(status))
+    {
+        int sig = WTERMSIG(status);
+        if (sig == SIGALRM)
+        {
+            if (verbose)
+                printf("Bad function: timed out after %u seconds\n", timeout);
+            return 0;
+        }
+        else
+        {
+            if (verbose)
+                printf("Bad function: terminated by signal %s\n", strsignal(sig));
+            return 0;
+        }
+    }
+    return -1;
 }
+
 
 void bad_function(void)
 {
@@ -91,6 +96,16 @@ void good_function(void)
 {
 }
 
+void cancel_alarm(void)
+{
+	struct sigaction ca;
+	ca.sa_handler = SIG_IGN;
+	sigaction(SIGALRM, &ca, NULL);
+
+	sleep(5);
+	printf("f waited 5 seconds, should be terminated before by alarm set in parent process if timeout < 5\n");
+}
+
 int main()
 {
     printf("Testing a good function\n");
@@ -98,6 +113,6 @@ int main()
 
     printf("Testing a bad function\n");
     sandbox(bad_function, 3, true);
-
+    sandbox(cancel_alarm, 3, true);
     return (0);
 }
