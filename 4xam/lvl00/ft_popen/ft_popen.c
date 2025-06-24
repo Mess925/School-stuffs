95db94339b2be2e2d4ce4def49dc41a220a8add3
Mess925 <hanminthant222@gmail.com>
2025-06-25 05:33:39 +0800

    exam ft_popen update

--- 4xam/lvl00/ft_popen/ft_popen.c
+++ 4xam/lvl00/ft_popen/ft_popen.c
@@ -1,4 +1,3 @@
-
 #include <unistd.h>
 #include <stdlib.h>
 
@@ -39,3 +38,38 @@ int	ft_popen(const char *file, char *const argv[], char type)
 	}
 	return -1;
 }
+// #include <fcntl.h>
+// #include <stdio.h>
+// #include <string.h>
+
+// // int main()
+// // {
+// // 	//int fd = open("texte", O_RDONLY);
+// // 	int fd = ft_popen("ls", (char *const[]){"ls", NULL}, 'r');
+
+// // 	char buf[1];
+// // 	while(read(fd, buf, 1))
+// // 		write(1, buf, 1);
+
+// // 	close(fd);
+// // 	return (0);
+// // }
+
+// //test type 'w'
+// int main()
+// {
+//     int fd = ft_popen("wc", (char *const[]){"wc", NULL}, 'w');
+// 	if (fd == -1)
+// 	{
+// 		perror("error ft_popen args");
+// 		return (0);
+// 	}
+
+//     char *input = "Hello world\nThis is a test\nthird line mofo\n";
+//     write(fd, input, strlen(input));
+	
+
+//     close(fd);
+// 	printf("after test\n");
+//     return (0);
+// }
