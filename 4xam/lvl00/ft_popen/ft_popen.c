f5269f260f601b688adb5416c2431b0f6a27cac7
Mess925 <hanminthant222@gmail.com>
2025-06-25 05:25:04 +0800

    exam ft_popen update

--- 4xam/lvl00/ft_popen/ft_popen.c
+++ 4xam/lvl00/ft_popen/ft_popen.c
@@ -1,47 +1,41 @@
-# include <stdio.h>
-# include <unistd.h>
-# include <stdlib.h>
-# include <sys/wait.h>
-# include <sys/types.h>
 
-int ft_popen(const char *file, const char *argv[], char type)
+#include <unistd.h>
+#include <stdlib.h>
+
+int	ft_popen(const char *file, char *const argv[], char type)
 {
-    if(!file || !argv || (type != 'r' && type != 'w'))
-        return (-1);
-    int fd[2];
-    if(pipe(fd) == -1)
-        return (-1);
-    pid_t pid = fork();
-    if(pid = -1)
-        return (-1);
-    if(pid == 0)
-    {
-        if(type = 'r')
-        {
-            close(fd[0]);
-            if(dup2(fd[1], STDOUT_FILENO) == -1)
-                exit (1);
-            close(fd[1]);
-        }
-        else
-        {
-            close (fd[1]);
-            if(dup2(fd[0], STDIN_FILENO) == -1)
-                exit (1);
-            close (fd[0]);
-        }
-        
-        execvp(file, argv);
-        exit(127);
-    }
-    if(type == 'r')
-    {
-        close(fd[1]);
-        return (fd[0]);
-    }
-    else
-    {
-        close(fd[0]);
-        return (fd[1]);
-    }
+	if (!file || !argv || (type != 'r' && type != 'w'))
+		return -1;
+
+	int fd[2];
+	int status;
+
+	pipe(fd);
+	if (type == 'r')
+	{
+		if (fork() == 0)
+		{
+			dup2(fd[1], STDOUT_FILENO);
+			close(fd[0]);
+			close(fd[1]);
+			execvp(file, argv);
+			exit (-1);
+		}
+		close(fd[1]);
+		return (fd[0]);
+	}
+	if (type == 'w')
+	{
+		if (fork() == 0)
+		{
+			dup2(fd[0], STDIN_FILENO);
+			close(fd[0]);
+			close(fd[1]);
+			execvp(file, argv);
+			exit (-1);
+		}
+		close(fd[0]);
+		return (fd[1]);
+	}
+	return -1;
 }
