b29eedf9ac9bf3dc7c68760ae02d686cfb48f217
Mess925 <hanminthant222@gmail.com>
2025-08-31 20:27:53 +0800

    ex02 finish

--- CPP/M05/ex02/RobotomyRequestForm.cpp
+++ CPP/M05/ex02/RobotomyRequestForm.cpp
@@ -6,7 +6,7 @@
 /*   By: hthant <hthant@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/30 17:45:45 by hthant            #+#    #+#             */
-/*   Updated: 2025/08/31 20:05:33 by hthant           ###   ########.fr       */
+/*   Updated: 2025/08/31 20:12:07 by hthant           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,12 @@ RobotomyRequestForm::RobotomyRequestForm(std::string target)
 void	RobotomyRequestForm::execute(Bureaucrat const &executor) const {
 	checkExecution(executor);
 	std::cout << "Drilling Noises" <<std::endl;
+	static bool seeded = false;
+	if(!seeded)
+	{
+		std::srand(std::time(NULL));
+		seeded = true;
+	}
 	if(std::rand() % 2)
 		std::cout<< _target << "has been robotomized successfully" <<std::endl;
 	else
