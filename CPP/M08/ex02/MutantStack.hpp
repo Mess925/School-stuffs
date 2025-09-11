6fec7d8834de51408dde6a99c7ce2bf3a2cd41a6
Mess925 <hanminthant222@gmail.com>
2025-09-11 12:28:26 +0800

    M08 ex02 started

---
+++ CPP/M08/ex02/MutantStack.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hthant <hthant@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/11 12:12:06 by hthant            #+#    #+#             */
+/*   Updated: 2025/09/11 12:15:29 by hthant           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+# include <iostream>
+# include <string>
+# include <algorithm>
+# include <vector>
+
+class MutantStack {
+
+}
+
+# endif
