21069d592c4252741d6ae591163dd03213c7e2dc
Mess925 <hanminthant222@gmail.com>
2025-09-15 13:09:21 +0800

    cpp09 ex00

--- CPP/M09/ex00/BitcoinExchange.cpp
+++ CPP/M09/ex00/BitcoinExchange.cpp
@@ -6,9 +6,32 @@
 /*   By: hthant <hthant@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/12 11:15:17 by hthant            #+#    #+#             */
-/*   Updated: 2025/09/12 11:17:37 by hthant           ###   ########.fr       */
+/*   Updated: 2025/09/12 14:45:35 by hthant           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "BitcoinExchange.hpp"
 
+BitcoinExchange::BitcoinExchange(){}
+
+BitcoinExchange::BitcoinExchange(const BitcoinExchange& other){
+	this->_money = other._money;
+}
+
+BitcoinExchange& BitcoinExchange::operator=(const BitcoinExchange& other){
+	if(this != &other)
+		this->_money = other._money;
+	return *this;
+}
+
+BitcoinExchange::~BitcoinExchange(){};
+
+bool BitcoinExchange::isValidFile(const std::string &fileName){
+	std::ifstream file;
+	file.open(fileName.c_str());
+	if(!file.is_open())
+		std::cout << "Cannot open the .txt file" << std::endl;
+	else
+		std::cout << "IT is valid" << std::endl;
+	return true;
+}
--- CPP/M09/ex00/BitcoinExchange.hpp
+++ CPP/M09/ex00/BitcoinExchange.hpp
@@ -6,19 +6,28 @@
 /*   By: hthant <hthant@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/12 10:55:22 by hthant            #+#    #+#             */
-/*   Updated: 2025/09/12 11:15:03 by hthant           ###   ########.fr       */
+/*   Updated: 2025/09/12 14:47:28 by hthant           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # ifndef BITCOINEXCHANGE_HPP
 # define BITCOINEXCHANGE_HPP
 
+# include <map>
+# include <iostream>
+# include <fstream>
+# include <string>
+
 class BitcoinExchange {
+	private:
+		std::map<std::string, double> _money;
 	public:
 		BitcoinExchange();
 		BitcoinExchange(const BitcoinExchange& other);
 		BitcoinExchange& operator=(const BitcoinExchange& other);
 		~BitcoinExchange();
+
+		bool isValidFile(const std::string &fileName);
 };
 
 # endif
