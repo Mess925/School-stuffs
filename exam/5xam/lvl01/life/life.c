491eac6dad58d365a0ee3a388bf8f52acb037c47
Mess925 <hanminthant222@gmail.com>
2025-12-08 20:54:45 +0800

    finish game of life

--- exam/5xam/lvl01/life/life.c
+++ exam/5xam/lvl01/life/life.c
@@ -10,6 +10,33 @@ void ft_free(int i, char **str){
 	free(str);
 }
 
+int aliveCount(char **str, int i, int j, int height, int width){
+	int count = 0;
+	for(int x = -1; x <= 1; x++){
+		for(int y = -1; y <= 1; y++){
+			if(x == 0 && y == 0){
+				continue;
+			}
+			int newx = i + x;
+			int newy = j + y;
+			if(newy >= 0 && newy < height && newx >= 0 && newx < width){
+				if(str[newx][newy] == 'O')
+					count++;
+			}
+		}
+	}
+	return count;
+}
+
+void	copy(char **dst, char **src, int height, int width){
+	for(int i = 0; i <height ; i++){
+		for(int j = 0; j < width; j++){
+			dst[i][j] = src[i][j];
+		}
+		dst[i][width] = '\0';
+	}
+}
+
 int main(int ac, char **av){
 	if(ac != 4)
 	{
@@ -21,12 +48,15 @@ int main(int ac, char **av){
 	int	iteration = atoi(av[3]);
 	char 	c;
 	char	**strr = malloc(sizeof(char*) * height);
+	char	**new = malloc(sizeof(char*) * height);
 	bool	pen =false;
 	if(!strr)
 		return -1;
 	for(int i = 0 ; i < height; i++){
 		strr[i] = malloc(sizeof(char) * (width + 1));
-		if(!strr[i]){
+		new[i] = malloc(sizeof(char) * (width + 1));
+		if(!strr[i] || !new[i]){
+			ft_free(i, new);
 			ft_free(i, strr);
 			return 0;
 		}
@@ -62,17 +92,43 @@ int main(int ac, char **av){
 				x++;
 		}
 		else {
+			ft_free(height, new);
 			ft_free(height, strr);
 			printf("%c is not supposed to be here\n", c);
 			return -1;
 		}
 		if(pen)
-			strr[y][x] = '#';
+			strr[y][x] = 'O';
+	}
+	while(iteration --){
+		for(int i = 0; i < height; i++){
+			for(int j = 0; j < width; j++){
+				int n = aliveCount(strr, i , j, height, width);	
+				if(strr[i][j] == 'O'){
+					if(n < 2)
+						new[i][j] = ' ';
+					else if ( n == 2 || n == 3 )
+						new[i][j] = 'O';
+					else
+						new[i][j] = ' ';
+				}
+				else{
+					if(n == 3){
+						new[i][j] = 'O';
+					}
+					else{
+						new[i][j] =' ';
+					}
+				}
+			}
+			new[i][width] = '\0';
+		}
+		copy(strr, new, height, width);
 	}
 	for(int i = 0; i < height; i++){
 		printf("%s\n", strr[i]);
 	}
 	ft_free(height, strr);
-	(void) iteration;
+	ft_free(height, new);
 	return 0;
 }
