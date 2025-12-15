7bde6fa18d1e9cd4c7dde53fe63b29b86a543dc8
Mess925 <hanminthant222@gmail.com>
2025-12-15 14:58:30 +0800

    good

--- exam/5xam/bsq/bsq.c
+++ exam/5xam/bsq/bsq.c
@@ -1,1 +1,53 @@
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
 
+void	error(FILE *stream , char *line){
+	fprintf(stderr, "ERROR: Invalid input\n");
+	if(stream){
+		fclose(stream);
+	}
+	if(line){
+		free(line);
+	}
+	exit(EXIT_FAILURE);
+}
+
+void getinput(FILE *stream){
+	ssize_t	nread;
+	size_t	len = 0;
+	char	*line = NULL;
+	int	line_count;
+	char	empty;
+	char	obstacle;
+	char	fill;
+	char	newline;
+	int	char_count = -1;
+
+	int check =fscanf(stream, "%d%c%c%c%c", &line_count, &empty, &obstacle, &fill, &newline);
+	if(check != 5 || newline != '\n')
+		error(stream , line);
+	for(int i = 0; i < line_count ; ++i){
+		nread = getline(&line, &len, stream);
+		fprintf(stdout, "The length of the line is %zu\nthe line is %s", nread, line);
+	}
+}
+
+int main(int argc, char **argv){
+	FILE	*stream;
+
+	if(argc < 2)
+		stream = stdin;
+	else if (argc == 2)
+		stream = fopen(argv[1], "r");
+	else{
+		fprintf(stderr ,"ERROR: Invalid Input\n");
+		exit(EXIT_FAILURE);
+	}
+	if(stream == NULL){
+		fprintf(stderr, "ERROR: Invalid Input\n");
+		exit(EXIT_FAILURE);
+	}
+	getinput(stream);
+	return 0;
+}
